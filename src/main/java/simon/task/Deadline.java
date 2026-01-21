266810fff6cbba2f8c572f801aeaf45556ba792f
Jolene <jolenechong7@gmail.com>
2026-01-21 13:24:00 +0800

    feat: replace with DateTime, connect to events and deadlines

--- src/main/java/simon/task/Deadline.java
+++ src/main/java/simon/task/Deadline.java
@@ -1,26 +1,40 @@
 package simon.task;
 
-public class Deadline extends Task {
-    private String by;
+import simon.util.DateParser;
 
-    public Deadline(String title, String by) {
+import java.time.LocalDateTime;
+
+public class Deadline extends Task {
+    private LocalDateTime by;
+
+    public Deadline(String title, LocalDateTime by) {
         super(title);
         this.by = by;
     }
 
-    public Deadline(String title, String by, boolean isCompleted) {
+    public Deadline(String title, String by) {
+        this(title, DateParser.parse(by));
+    }
+
+    public Deadline(String title, LocalDateTime by, boolean isCompleted) {
         super(title);
         this.by = by;
         super.setCompleted(isCompleted);
     }
 
+    public Deadline(String title, String by, boolean isCompleted) {
+        this(title, DateParser.parse(by), isCompleted);
+    }
+
     @Override
     public String toDataString() {
+        // format date to  2nd of December 2019, 6pm
+
         return "D | " + (isCompleted() ? "1" : "0") + " | " + super.getTitle() + " | " + this.by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + DateParser.format(this.by) + ")";
     }
 }
